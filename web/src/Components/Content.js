5271e15c187eeae6d360daf274d0a5c6b7657c0b
reiven <seventhtrigger0@gmail.com>
2017-11-02 16:53:37 -0500

    add Modal Component and remove functionality

--- web/src/Components/Content.js
+++ web/src/Components/Content.js
@@ -3,12 +3,18 @@ import './Style.css';
 
 import Pin from './Pin';
 import Card from "./Card";
+import Modal from "./Modal";
 
 class Content extends Component {
   render() {
     return (
         <div>
             <Pin />
+            <Modal active={false}/>
+            {/*
+             active version is here
+             <Modal active={true}/>
+             */}
             <div className="columns is-multiline">
                 <Card cardTitle="title" />
 
---
+++ web/src/Components/Modal.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+class ModalContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+
+        this.state.active = this.props.active;
+        this.hideModal = this.hideModal.bind(this);
+    }
+
+    hideModal(){
+        this.setState({"active": false});
+    }
+
+    render(){
+        return (
+            <div className={this.state.active? "modal is-active": "modal"}>
+                <div className="modal-background"></div>
+                <div className="modal-card">
+                    <header className="modal-card-head">
+                        <p className="modal-card-title">{this.props.title}</p>
+                        <button className="delete" aria-label="close" onClick={this.hideModal}>
+                        </button>
+                    </header>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+class ModalCardBody extends Component {
+    render(){
+        return (
+            <section className="modal-card-body">
+                {this.props.children}
+            </section>
+        )
+    }
+}
+
+class ModalCardFooter extends Component {
+    render(){
+        return (
+            <footer className="modal-card-foot">
+                {/*temporary same as example,
+                 https://bulma.io/documentation/components/modal/
+                 */}
+                <button className="button is-success">Save changes</button>
+                <button className="button">Cancel</button>
+            </footer>
+        )
+    }
+}
+
+class Modal extends Component {
+    constructor(props){
+        super(props);
+        if(typeof props.active === "undefined"){
+            props.active = false;
+        }
+    }
+    render(){
+        return (
+            <ModalContainer active={this.props.active}>
+                <ModalCardBody>
+                    This is the main part of the modal
+                </ModalCardBody>
+                <ModalCardFooter/>
+            </ModalContainer>
+        );
+    }
+}
+
+export default Modal;
