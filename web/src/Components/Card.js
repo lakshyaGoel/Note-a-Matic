6da48d9fc8e9f0902d1b8f9f9ba8f61ac32768c4
reiven <seventhtrigger0@gmail.com>
2017-11-02 16:08:19 -0500

    Add Static Card Component

---
+++ web/src/Components/Card.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+
+function isSet(item){
+    if(typeof item == "undefined"){
+        return false;
+    }else{
+        return true;
+    }
+}
+class CardHeadImage extends Component {
+    render(){
+        return (
+            <div className="card-image">
+                <figure className="image is-4by3">
+                    <img src={this.props.imgURL} alt="Placeholder image"/>
+                </figure>
+            </div>
+        );
+    }
+}
+
+class CardHeader extends Component {
+    render(){
+        return (
+            <header className="card-header">
+                <p className="card-header-title">
+                    {this.props.title}
+                </p>
+            </header>
+        );
+    }
+}
+
+class CardContent extends Component {
+    render(){
+        return (
+            <div className="card-content">
+                <div className="content">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+class CardFooter extends Component{
+    render(){
+        return (
+            <footer className="card-footer">
+                {/* basic item goes here
+                    temporary, I add save edit delete button same as example
+                    https://bulma.io/documentation/components/card/
+                */}
+                <a className="card-footer-item">Save</a>
+                <a className="card-footer-item">Edit</a>
+                <a className="card-footer-item">Delete</a>
+            </footer>
+        );
+    }
+}
+
+class CardContainer extends Component {
+    render(){
+        return (
+            <div className="column is-one-quarter">
+                <div className="card">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Card extends Component{
+    constructor(props){
+       super(props);
+
+    }
+    render(){
+        return (
+            <CardContainer>
+                <CardHeader title={this.props.cardTitle}/>
+                <CardContent>
+                    This is inside card. main content.
+                </CardContent>
+                <CardFooter />
+            </CardContainer>
+        );
+    }
+}
+
+export default Card;
--- web/src/Components/Content.js
+++ web/src/Components/Content.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './Style.css';
 
 import Pin from './Pin';
+import Card from "./Card";
 
 class Content extends Component {
   render() {
     return (
         <div>
-            <Pin/>
+            <Pin />
+            <div className="columns is-multiline">
+                <Card cardTitle="title" />
+            </div>
         </div>
     );
   }
