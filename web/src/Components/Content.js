b04ce6c9b5dc4382a4643a5e90b85f0df21a6383
reiven <seventhtrigger0@gmail.com>
2017-11-02 16:09:40 -0500

    Commit to activate my branch

--- web/src/Components/Content.js
+++ web/src/Components/Content.js
@@ -11,6 +11,7 @@ class Content extends Component {
             <Pin />
             <div className="columns is-multiline">
                 <Card cardTitle="title" />
+                Temporary Commit!
             </div>
         </div>
     );
