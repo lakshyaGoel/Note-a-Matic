26d070d6e05638aecf69c21cac6fbb46e98c70d2
Lakshya Goel <goel0072@umn.edu>
2017-11-02 15:19:27 -0500

    Authorization inclusion

--- web/src/index.js
+++ web/src/index.js
@@ -5,30 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.css';
 
-// import App from './App';
+import App from './App';
 
-// ReactDOM.render((
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// ), document.getElementById('root'));
-
-import Header from './Components/Header';
-import LeftSideBar from './Components/LeftSideBar';
-import Content from './Components/Content';
-import RightSideBar from './Components/RightSideBar';
-import Footer from './Components/Footer';
-
-ReactDOM.render(
-    <div>
-        <Header/>
-        <div className="ContentArea">
-            <div style={{"display":"table-row"}}>
-                <LeftSideBar />
-                <Content />
-                <RightSideBar/>
-            </div>
-        </div>
-        <Footer/>
-    </div>
-    , document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
