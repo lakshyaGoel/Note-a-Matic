6552cad8e9bc91a7e05a20fee820459f8a3ab62c
Vaybhav Shaw <vaybhavshaw@gmail.com>
2017-11-03 14:48:37 -0500

    Adding code removed by mistake

--- web/src/Components/LeftSideBar.js
+++ web/src/Components/LeftSideBar.js
@@ -14,7 +14,12 @@ class LeftSideBar extends Component {
     return (
         <div className="customLeftSideBar">
             <p>LeftSideBar</p>
+            <img className="img-circle" src={this.props.profile.picture} alt="Placeholder image" />
+            <div><a className="centre-this" class="is-small is-active" href="#" onclick="gohere();return false;">{this.props.profile.nickname}</a></div>
             
+            <div><a className="centre-this is-active" id="myLink" href="#" onclick="gohere();return false;">All Notes</a></div>
+            <div><a id="myLink" title="Click to do something" href="#" onclick="gohere();return false;">My Notes</a></div>
+            <div><a className="centre-this" id="myLink" href="#" onclick="gohere();return false;">Shared Notes</a></div>
         
 
  
