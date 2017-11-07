017f4c8ec25060bf316093c01eb2d32d0a190137
reiven <seventhtrigger0@gmail.com>
2017-11-07 14:19:27 -0600

    make mongoose schema.(beta: need to test)

---
+++ server/model/Tags.js
@@ -0,0 +1,18 @@
+/**
+ * Created by reiven on 2017/11/07.
+ */
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+
+var tags = new mongoose.Schema({
+    id: Schema.Types.ObjectId,
+    tagName: String, // img url
+    noteId: [],
+    createdAt: {type: Date, default: Date.now }//TODO: need to check. same as Model/Note.js
+});
+
+
+mongoose.connect(process.env.DB_URL);
+
+module.exports = mongoose.model("Tags", tags);
