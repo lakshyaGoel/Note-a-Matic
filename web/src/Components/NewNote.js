2d1fa97d103a25139e5bd18d592e50ebf7df5d65
Lakshya Goel <goel0072@umn.edu>
2017-11-12 14:50:55 -0600

    Edit with bugs

--- web/src/Components/NewNote.js
+++ web/src/Components/NewNote.js
@@ -66,6 +66,7 @@ class NewNote extends Component {
         var type = this._reactInternalFiber._debugOwner.stateNode.props.match.params.type;
         var noteId = this._reactInternalFiber._debugOwner.stateNode.props.match.params.noteId;
         if (noteId) { // when editing, not creating new note.
+            this.setState({noteType: type});
             let request = new Request('/api/db/get-note', {
                 method: 'POST',
                 headers: {
@@ -82,14 +83,34 @@ class NewNote extends Component {
                 }
                 return response.json();
             }).then(res => {
-                // TODO: Tag data fix!
                 if (res) {
-                    console.log(res);
-                    // TODO: Lakshya: fix here to show content properly.
-                    this.setState({noteType: type});
+                    console.log(res.note.content);
+                    document.getElementById("noteTags").setAttribute("disabled", "");
+                    this.setState({
+                        private: !res.note.share,
+                        noteType: res.note.type,
+                        noteTitle: res.note.title,
+                        noteDesc: res.note.description,
+                        noteCont: res.note.content,
+                        tags: [],
+                        shared: [],
+                        userID: this.props.profile.name,
+                        lastEdit: this.props.profile.name
+                    })
+                    if (res.note.type === "Code") {
+                        this.setState({
+                            editorProp: {
+                                mode: res.note.codeSetting.mode,
+                                theme: res.note.codeSetting.theme,
+                                autoComplete: res.note.codeSetting.autoComplete,
+                                lineNumber: res.note.codeSetting.lineNumber
+                            }
+                        });
+                    }
                 }
             });
-        } else { // creating new note
+        } else {
+            document.getElementById("noteTags").removeAttribute("disabled");
             this.setState({noteType: type});
         }
     }
@@ -129,6 +150,7 @@ class NewNote extends Component {
         this.setState({noteCont: v.value, mode: v.mode, theme: v.theme, autoComplete: v.enableLiveAutocompletion, lineNumber: v.showLineNumbers});
     }
     render() {
+        console.log(this.state.noteCont);
         if (!this.state.redirect) {
             return (
                 <div className="NoteClass">
@@ -159,13 +181,13 @@ class NewNote extends Component {
                         </div>
                     </div>
                     {this.state.noteType === "Text"
-                        ? <TextNote onEditDesc={this.updateDesc}/>
+                        ? <TextNote content={this.state.noteCont} onEditDesc={this.updateDesc}/>
                         : <CodeNote
                             mode={this.state.editorProp.mode}
                             theme={this.state.editorProp.theme}
                             autoComplete={this.state.editorProp.autoComplete}
                             lineNumber={this.state.editorProp.lineNumber}
-                            defaultNoteContent={defaultValue}
+                            defaultNoteContent={this.state.noteCont}
                             onCodeUpdates={this.onCodeUpdates}/>}
                     <div className="field">
                         <label className="label">#Tags</label>
@@ -176,6 +198,7 @@ class NewNote extends Component {
                                 className="input"
                                 name="tags"
                                 type="text"
+                                id="noteTags"
                                 placeholder="Enter the tags starting with # seperated by ','"/>
                         </div>
                     </div>
@@ -236,7 +259,7 @@ class TextNote extends Component {
             .onChange
             .bind(this);
         this.state = {
-            desc: ""
+            desc: props.content
         };
     }
 
@@ -249,13 +272,13 @@ class TextNote extends Component {
     render() {
         return (
             <div className="field">
-                <label className="label">Note</label>
+                <label className="label">Note;</label>
                 <div className="control">
                     <textarea
                         value={this.state.desc}
                         onChange={this.onChange}
                         className="textarea"
-                        placeholder="Write what you want to save..."></textarea>
+                        placeholder="Write what you want to save...">{this.props.content}</textarea>
                 </div>
             </div>
         );
